b363086d997f135d06848a809ad68f6c1a66604d
hhhc0213 <hhhc0213@naver.com>
2022-09-30 17:55:28 +0900

    스투시 ACCOUNT html, css, js 클론코딩(FOOTER 제외)

---
+++ src/main/java/com/stussy/stussyclone20220903SHC/StussyClone20220903ShcApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220903SHC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220903ShcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220903ShcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stussy/stussyclone20220903SHC/StussyClone20220903ShcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220903SHC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220903ShcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
